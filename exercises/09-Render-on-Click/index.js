05c1be5291836fd8474f719c7f9b9ff3cec60d94
Daniel Martinez <d.martinez74@gmail.com>
2023-03-17 23:20:01 +0000

    Done until exercise 10

--- exercises/09-Render-on-Click/index.js
+++ exercises/09-Render-on-Click/index.js
@@ -1,5 +1,8 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
-	//your code here
-	
+    //your code here
+    let element = document.createElement("div");
+    element.style.background = "yellow";
+    element.innerHTML = "Hello World";
+    document.body.appendChild(element);
 });
--- exercises/10-Add-li-on-Click/index.js
+++ exercises/10-Add-li-on-Click/index.js
@@ -1,5 +1,9 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
-	//your code here
+    //your code here
+    let list = document.getElementById("myList");
+    let child = document.createElement("li");
+    child.appendChild(document.createTextNode("Forth element"));
+    list.appendChild(child);
 
 });

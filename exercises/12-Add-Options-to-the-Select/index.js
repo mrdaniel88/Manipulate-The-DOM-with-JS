5a034baf4fc4ca4700bad0eec3b74404670e17a8
Daniel Martinez <d.martinez74@gmail.com>
2023-03-18 03:01:38 +0000

    Done until exercise 12

--- exercises/12-Add-Options-to-the-Select/index.js
+++ exercises/12-Add-Options-to-the-Select/index.js
@@ -1,2 +1,20 @@
 let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 // your code here
+
+
+
+let select = document.getElementById("mySelect");
+
+for (let i = 0; i < countries.length; i++) {
+    let country = document.createElement("option");
+    country.value = i
+    country.text = countries[i]
+    select.appendChild(country)
+}
+
+
+
+select.addEventListener("change", function(e) {
+
+    alert(countries[e.target.value])
+})
